a9ed1d5639c9d694ddd5ecb5f7902350f41d1d93
rradev <rossendom@yahoo.com>
2017-01-20 17:35:59 +0200

    added locale.

---
+++ src/main/java/net/iosynth/sensor/SensorCountry.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package net.iosynth.sensor;
+
+import java.util.Locale;
+
+import net.iosynth.gen.GeneratorCountry;
+
+/**
+ * @author rradev
+ *
+ */
+public class SensorCountry extends Sensor {
+	private transient GeneratorCountry gen;
+	private Locale locale;
+	/**
+	 * 
+	 */
+	public SensorCountry() {
+		this.locale = Locale.US;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.iosynth.sensor.Sensor#step(long)
+	 */
+	@Override
+	public void step(long step) {
+		// nothing to do
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.iosynth.sensor.Sensor#getString()
+	 */
+	@Override
+	public String getString() {
+		return "\"" + gen.getCountry() + "\"";
+	}
+
+	/* (non-Javadoc)
+	 * @see net.iosynth.sensor.Sensor#checkParameters()
+	 */
+	@Override
+	public void checkParameters() {
+		// nothing to do
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.iosynth.sensor.Sensor#replicate()
+	 */
+	@Override
+	public void replicate() {
+		gen = new GeneratorCountry(rnd, locale);
+	}
+
+}
