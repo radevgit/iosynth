9b6b4183174aec972f39cb5ce37622021b2746f2
rradev <rossendom@yahoo.com>
2017-01-20 16:23:45 +0200

    ipv4 generator.

---
+++ src/main/java/net/iosynth/util/GeneratorIPv4.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package net.iosynth.util;
+
+import java.util.logging.Logger;
+
+import net.iosynth.device.Device;
+
+/**
+ * @author rradev
+ *
+ */
+public class GeneratorIPv4 {
+	private Xoroshiro128 rnd;
+	private int s;
+	private final int state[] = new int[4];
+	private static final String dot = ".";
+	private final Logger logger = Logger.getLogger(GeneratorIPv4.class.getName());
+	
+	/**
+	 * @param rnd 
+	 * @param prefix 
+	 * 
+	 */
+	public GeneratorIPv4(Xoroshiro128 rnd, String prefix) {
+		this.rnd = rnd;
+		if (prefix == null || prefix.length() == 0) {
+			s = 0;
+		} else {
+			if (prefix.charAt(prefix.length() - 1) == '.') {
+				prefix = prefix.substring(0, prefix.length() - 1); // remove dot at the end
+			}
+			String parts[] = prefix.split("\\.", 4);
+			s = parts.length;
+			for (int i = 0; i < parts.length; i++) {
+				try {
+					state[i] = Integer.parseInt(parts[i]);
+					if (state[i] > 255) {
+						throw new NumberFormatException();
+					}
+				} catch (NumberFormatException e) {
+					logger.severe("This is not a valid IPv4 prefix: " + parts[i]);
+					throw e;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @return random IPv4
+	 */
+	protected String getRandomIPv4() {
+		switch (s) {
+		case 0:
+			do {
+				state[0] = rnd.nextInt(254);
+			} while (state[0] == 0 || state[0] == 10 || state[0] == 127 || state[0] == 172 || state[0] == 192 || state[0] == 224);
+		case 1:
+			state[1] = rnd.nextInt(256);
+		case 2:
+			state[2] = rnd.nextInt(256);
+		case 3:
+			state[3] = rnd.nextInt(256);
+			break;
+		default:
+			break;
+		}
+		return format();
+	}
+	
+	/**
+	 * @return next IPv4
+	 */
+	public String getNextIPv4(){
+		state[3] += 1;
+		if (state[3] > 254) {
+			state[3] = 0;
+			state[2] += 1;
+			if (state[2] > 254) {
+				state[2] = 0;
+				state[1] += 1;
+				if (state[1] > 254) {
+					state[1] = 0;
+				}
+			}
+		}
+		return format();
+	}
+	
+	protected String format(){
+		StringBuilder b = new StringBuilder(15);
+		b.append(state[0]).append(dot)
+		.append(state[1]).append(dot)
+		.append(state[2]).append(dot)
+		.append(state[3]);
+		return b.toString();
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// performance: 10^9 getRandomIPv4() = 136s
+		Xoroshiro128 rnd = new Xoroshiro128(123);
+		GeneratorIPv4 gen = new GeneratorIPv4(rnd, "");
+		long start = System.currentTimeMillis();
+		for(int i=0; i<1000000000; i++){
+			gen.getRandomIPv4();
+			//System.out.println(gen.getRandomIPv4());
+		}
+		long end = System.currentTimeMillis();
+		System.out.println(String.valueOf(end-start));
+	}
+
+}
---
+++ src/main/java/net/iosynth/util/GeneratorStaticIPv4.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package net.iosynth.util;
+
+/**
+ * @author rradev
+ *
+ */
+public final class GeneratorStaticIPv4 {
+	/*
+	 * RNG used by this class to create random based IPv4. In a holder class to
+	 * defer initialization.
+	 */
+	private static class Holder {
+		static final GeneratorIPv4 rnd = new GeneratorIPv4(new Xoroshiro128(System.currentTimeMillis()), null);
+	}
+
+	/**
+	 * @return IPv4
+	 */
+	public static String getRandomIPv4() {
+		return Holder.rnd.getRandomIPv4();
+	}
+
+	/**
+	 * @return IPv4
+	 */
+	public static String getNextIPv4() {
+		return Holder.rnd.getNextIPv4();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GeneratorStaticIPv4.getRandomIPv4();
+		for (int i = 0; i < 300; i++) {
+			System.out.println(GeneratorStaticIPv4.getNextIPv4());
+		}
+	}
+
+}
