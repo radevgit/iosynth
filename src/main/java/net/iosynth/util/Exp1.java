cc0657f19380fe63bfb9887ebb9388f2d205a39e
rradev <rossendom@yahoo.com>
2017-01-28 09:32:14 +0200

    config fix.

--- src/main/java/net/iosynth/util/Exp1.java
+++ src/main/java/net/iosynth/util/Exp1.java
@@ -4,23 +4,38 @@
 package net.iosynth.util;
 
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.iosynth.gen.GeneratorString;
+import net.iosynth.gen.Xoroshiro128;
 
 /**
  * @author rradev
  *
  */
 public class Exp1 {
+
+	public Exp1() {
+
+	}
 	
 	public void sss(){
-		Random rnd = new Random();
-		rnd.nextGaussian();
+		String topic = "aaaaa/{$uuid}/bbbbb";
+		
+		Pattern p = Pattern.compile("\\{\\$uuid}");
+		String res = p.matcher(topic).replaceAll("xxx");
+		System.out.println(res);
 	}
+
 
 	/**
-	 * 
+	 * @param args
 	 */
-	public Exp1() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		Exp1 e = new Exp1();
+		e.sss();
 	}
 
+	
 }
